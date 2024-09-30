f0b2ba18799f8b260bdb29d4127efaa2f746d7f3
pawe213 <pawe213@gmail.com>
2024-09-30 03:15:03 +0200

    SCRUM-8 - add bielik

--- LAI/src/apps/app.py
+++ LAI/src/apps/app.py
@@ -14,6 +14,7 @@ from models.base_llm_model import LLMModel
 from models.base_tokenizer import Tokenizer
 from models.embedding_model.sentence_transformer_model import SentenceTransformerModel
 from models.llm.hugging_face_llm import HuggingFaceLLM
+from models.propmpts.prompt_register import prompts
 from models.tokenizer.auto_tokenizer import AutoTokenizerModel
 
 
@@ -45,12 +46,13 @@ class AlphaLAI(BaseApp):
         return SpacyNLPTextPreprocessor(self.language)
 
     def _get_tokenizer(self) -> AutoTokenizerModel:
-        return AutoTokenizerModel("google/gemma-2b-it")
+        return AutoTokenizerModel("speakleash/Bielik-11B-v2.3-Instruct")
 
     def _get_llm_model(self) -> HuggingFaceLLM:
         huggingface_hub.login(token='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')
         bnb_config = BitsAndBytesConfig(load_in_4bit=True)
-        return HuggingFaceLLM("google/gemma-2b-it", torch.float16, bnb_config, tokenizer=self._get_tokenizer())
+        return HuggingFaceLLM("speakleash/Bielik-11B-v2.3-Instruct",
+                              torch.float16, bnb_config, tokenizer=self._get_tokenizer())
 
     def load_file(self, file_path: str) -> list[TextPage]:
         file_loader = self._get_input_data_loader()
@@ -82,25 +84,8 @@ class AlphaLAI(BaseApp):
     def prepare_prompt(self, query: str, context_items: list[str]) -> str:
         context = "- " + "\n- ".join([extracted_paragraph for extracted_paragraph in context_items])
 
-        base_prompt = """Based on the following context items, please answer the query.
-        Give yourself room to think by extracting relevant passages from the context before answering the query.
-        Don't return the thinking, only return the answer.
-        Make sure your answers are as explanatory as possible.
-        Use the following examples as reference for the ideal answer style.
-        \nExample 1:
-        Query: What are the fat-soluble vitamins?
-        Answer: The fat-soluble vitamins include Vitamin A, Vitamin D, Vitamin E, and Vitamin K. These vitamins are absorbed along with fats in the diet and can be stored in the body's fatty tissue and liver for later use. Vitamin A is important for vision, immune function, and skin health. Vitamin D plays a critical role in calcium absorption and bone health. Vitamin E acts as an antioxidant, protecting cells from damage. Vitamin K is essential for blood clotting and bone metabolism.
-        \nExample 2:
-        Query: What are the causes of type 2 diabetes?
-        Answer: Type 2 diabetes is often associated with overnutrition, particularly the overconsumption of calories leading to obesity. Factors include a diet high in refined sugars and saturated fats, which can lead to insulin resistance, a condition where the body's cells do not respond effectively to insulin. Over time, the pancreas cannot produce enough insulin to manage blood sugar levels, resulting in type 2 diabetes. Additionally, excessive caloric intake without sufficient physical activity exacerbates the risk by promoting weight gain and fat accumulation, particularly around the abdomen, further contributing to insulin resistance.
-        \nExample 3:
-        Query: What is the importance of hydration for physical performance?
-        Answer: Hydration is crucial for physical performance because water plays key roles in maintaining blood volume, regulating body temperature, and ensuring the transport of nutrients and oxygen to cells. Adequate hydration is essential for optimal muscle function, endurance, and recovery. Dehydration can lead to decreased performance, fatigue, and increased risk of heat-related illnesses, such as heat stroke. Drinking sufficient water before, during, and after exercise helps ensure peak physical performance and recovery.
-        \nNow use the following context items to answer the user query:
-        {context}
-        \nRelevant passages: <extract relevant passages from the context here>
-        User query: {query}
-        Answer:"""
+
+        base_prompt = prompts['bielik']
         base_prompt = base_prompt.format(context=context,
                                          query=query)
         tokenizer = self._get_tokenizer()
